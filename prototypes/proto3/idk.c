8486adb82beebe94d9660bcfe5bc16ccb4e03c44
assballs <ass@balls>
2024-07-13 11:37:16 -0600

    movement testing

--- prototypes/proto3/idk.c
+++ prototypes/proto3/idk.c
@@ -1,75 +1,17 @@
 #include "raylib.h"
 
+// Using this to figure out how to handle input while moving
+//
+
 #define screenWidth 800
 #define screenHeight 450
 
-double previousTime;	
-double currentTime;	
-double elapsed;	
-double lag;	
-double time;
-double moveSpeed;
-// double t = 0.0;
-// double dt = 0.01;
-//
-// double currentTime = hires_time_in_seconds();
-// double accumulator = 0.0;
-//
-// State previous;
-// State current;
-//
-// while ( !quit )
-// {
-//     double newTime = time();
-//     double frameTime = newTime - currentTime;
-//     if ( frameTime > 0.25 )
-//         frameTime = 0.25;
-//     currentTime = newTime;
-//
-//     accumulator += frameTime;
-//
-//     while ( accumulator >= dt )
-//     {
-//         previousState = currentState;
-//         integrate( currentState, t, dt );
-//         t += dt;
-//         accumulator -= dt;
-//     }
-//
-//     const double alpha = accumulator / dt;
-//
-//     State state = currentState * alpha +
-//         previousState * ( 1.0 - alpha );
-//
-//     render( state );
-// }
-void updateGame() {
-    TraceLog(LOG_INFO, "updating game");
+void updateGame(void) {
+    // TraceLog(LOG_INFO, "updating game");
 }
 
-void renderGame(double alpha) {
-    TraceLog(LOG_INFO, "rendering game: alpha: %f", alpha);
-}
-
-void advanceTime(void) {
-    time += 0.15;
-    TraceLog(LOG_INFO, "time: %f", time);
-}
-
-void doTick() {
-    previousTime = currentTime;	
-    currentTime = time;
-    elapsed = currentTime - previousTime;	
-    lag += elapsed;
-    TraceLog(LOG_INFO, "prev: %f, curr: %f, elapsed: %f, lag: %f", previousTime, currentTime, elapsed, lag);
-
-    if (lag >= moveSpeed) {
-        lag -= moveSpeed;
-        TraceLog(LOG_INFO, "lag: %f", lag);
-        updateGame();
-    }
-
-    renderGame(lag / moveSpeed);
+void renderGame(void) {
+    // TraceLog(LOG_INFO, "rendering game");
 }
 
 int main(void) {
@@ -79,29 +21,11 @@ int main(void) {
     InitWindow(screenWidth, screenHeight, "idk figure out");
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
                                     
-    time = 0.0;
-    previousTime = time;	
-    currentTime = previousTime;	
-    elapsed = currentTime - previousTime;	
-    lag = 0.0;	
-    moveSpeed = 0.1;
-
-
-    // thinking if I set shit up, then use the space bar to do a 'tick' 
-    //
-
     while (!WindowShouldClose()) {   // Detect window close button or ESC key
 
-        if (IsKeyPressed(KEY_SPACE)) {
-            doTick();
-        }
-        if (IsKeyPressed(KEY_D)) {
-            advanceTime();
-        }
         BeginDrawing();
-
         ClearBackground(BLACK);
-
+        renderGame();
         EndDrawing();
 
     }
--- prototypes/proto3/movement.c
+++ prototypes/proto3/movement.c
@@ -1,44 +1,79 @@
+/*
+ * the smooth movement seems do be fine.  Although, it lags behind the other
+ * methods. 
+ *
+ * The issue then becomes with input coming in but the direction should not change
+ * unless the player is has entered a new grid square.  The player can do something like
+ * press left, and then while moving left, quickly press down then right, and the right,
+ * which shouldnt' work because the player cannot move over themself, but because at that
+ * point the player direction is down, this works fine.
+ *
+ * So what we need is a way to only change the direction once the player has completely 
+ * moved over to the next grid square.  
+ *
+ * How do we know when the player has completely moved over to the next grid square?
+ * can we just check the player position as an int, floor(pos) should work right?
+ * and then when floor(pos) != floor(prev) we have moved to a new grid square.  we
+ * can then process and input that has been captured while moving, and just 
+ * don't process anything at that point that is an illegal move
+ *
+ * So wait, aren't we already doing this?  We have two squares drawn already for the
+ * smooth movement, so we can just check if the player has moved over to the next grid
+ * How can we sync up the smooth movement to hit the grid square at the same time as
+ * the ones that snap?
+ */
+
+
 #include "raylib.h"
 
 #define RAYMATH_IMPLEMENTATION
 #include "raymath.h"
 
-#define GRID_SIZE 20
+#define GRID_SIZE 8 
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 800
+#define SCREEN_WIDTH 2*800
+#define SCREEN_HEIGHT 2*800
+
+#define FIXED_TIMESTEP 1.0
 
 struct {
     Vector2 pos;
     Vector2 dir;
-    double time;
-    double dt;
+    double accumulator;
 } simple_loop;
 
 struct {
     Vector2 currPos;
     Vector2 prevPos;
     Vector2 dir;
-    double t;
-    double dt;
     double currentTime;
     double accumulator;
 } continuous_loop;
 
+struct {
+    Vector2 pos;
+    Vector2 dir;
+    double accumulator;
+} hybrid_loop;
+
+double time;
+double frameTime;
+
+
 void initGame() {
+    time = GetTime();
     simple_loop.pos = (Vector2) {0, 1};
     simple_loop.dir = (Vector2) {1, 0};
-    simple_loop.time = 0;
-    simple_loop.dt = 0;
 
     continuous_loop.currPos = (Vector2) {0, 3};
     continuous_loop.prevPos = (Vector2) {0, 3};
     continuous_loop.dir = (Vector2) {1, 0};
-    continuous_loop.t = 0;
-    continuous_loop.dt = 0.2;
     continuous_loop.currentTime = GetTime();
     continuous_loop.accumulator = 0;
-    // TraceLog(LOG_INFO, "initializing game");
+
+    hybrid_loop.pos = (Vector2) {0, 5};
+    hybrid_loop.dir = (Vector2) {1, 0};
+    hybrid_loop.accumulator = 0;
 }
 
 // draw the grid.  split this grid into
@@ -53,10 +88,9 @@ void drawGrid() {
 }
 
 void simpleUpdateGame() {
-    simple_loop.dt = GetFrameTime();
-    simple_loop.time += simple_loop.dt;
-    if (simple_loop.time > 0.2) {
-        simple_loop.time -= 0.2;
+    simple_loop.accumulator += frameTime;
+    if (simple_loop.accumulator > FIXED_TIMESTEP) {
+        simple_loop.accumulator -= FIXED_TIMESTEP;
         simple_loop.pos.x += simple_loop.dir.x;
         simple_loop.pos.y += simple_loop.dir.y;
         if (simple_loop.pos.x <= 0) {
@@ -78,17 +112,16 @@ void simpleRenderGame() {
 void continuousUpdateGame(void) {
     double newTime = GetTime();
     double frameTime = newTime - continuous_loop.currentTime;
-    if ( frameTime > 0.25 )
-        frameTime = 0.25;
+    if ( frameTime > FIXED_TIMESTEP )
+        frameTime = FIXED_TIMESTEP;
     continuous_loop.currentTime = newTime;
     continuous_loop.accumulator += frameTime;
 
-    while (continuous_loop.accumulator >= continuous_loop.dt) {
+    while (continuous_loop.accumulator >= FIXED_TIMESTEP) {
         continuous_loop.prevPos = continuous_loop.currPos;
         continuous_loop.currPos.x += continuous_loop.dir.x;
         continuous_loop.currPos.y += continuous_loop.dir.y;
-        continuous_loop.t += continuous_loop.dt;
-        continuous_loop.accumulator -= continuous_loop.dt;
+        continuous_loop.accumulator -= FIXED_TIMESTEP;
         if (continuous_loop.currPos.x <= 0) {
             continuous_loop.dir.x *= -1;
             continuous_loop.currPos.x = 0;
@@ -102,10 +135,33 @@ void continuousUpdateGame(void) {
 void continuousRenderGame(void) {
     DrawRectangle(continuous_loop.currPos.x * SCREEN_WIDTH / GRID_SIZE, continuous_loop.currPos.y * SCREEN_HEIGHT / GRID_SIZE, SCREEN_WIDTH / GRID_SIZE, SCREEN_HEIGHT / GRID_SIZE, BLUE);
        
-    double alpha = continuous_loop.accumulator / continuous_loop.dt;
+    double alpha = continuous_loop.accumulator / FIXED_TIMESTEP;
     Vector2 pos = Vector2Lerp(continuous_loop.prevPos, continuous_loop.currPos, alpha);
     int square_size = SCREEN_WIDTH / GRID_SIZE;
     DrawRectangle(pos.x * square_size , pos.y * square_size, square_size, square_size, RED);
+}
+
+void hybridUpdateGame(void) {
+
+    hybrid_loop.accumulator += frameTime;
+
+    while (hybrid_loop.accumulator >= FIXED_TIMESTEP) {
+        hybrid_loop.pos.x += hybrid_loop.dir.x;
+        hybrid_loop.pos.y += hybrid_loop.dir.y;
+        hybrid_loop.accumulator -= FIXED_TIMESTEP;
+        if (hybrid_loop.pos.x <= 0) {
+            hybrid_loop.dir.x *= -1;
+            hybrid_loop.pos.x = 0;
+        } else if (hybrid_loop.pos.x >= GRID_SIZE - 1) {
+            hybrid_loop.dir.x *= -1;
+            hybrid_loop.pos.x = GRID_SIZE - 1;
+        }
+    }
+}
+
+void hybridRenderGame(void) {
+    int square_size = SCREEN_WIDTH / GRID_SIZE;
+    DrawRectangle(hybrid_loop.pos.x * square_size , hybrid_loop.pos.y * square_size, square_size, square_size, BLUE);
 }
 
 int main(void) {
@@ -116,18 +172,42 @@ int main(void) {
     SetTargetFPS(60);
                                     
     InitAudioDevice();
-
     initGame();
+
+    double time = GetTime();
+    // hang out and boot up bitches
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        drawGrid();
+        // draw FPS
+        DrawText(TextFormat("FPS: %d", GetFPS()), 10, 10, 20, GREEN);
+        EndDrawing();
+        if (GetTime() - time > FIXED_TIMESTEP) {
+            break;
+        }
+       
+    }
+
 
     while (!WindowShouldClose()) {   // Detect window close button or ESC key
                                      
+        double newTime = GetTime();
+        frameTime = newTime - continuous_loop.currentTime;
+        if ( frameTime > FIXED_TIMESTEP )
+            frameTime = FIXED_TIMESTEP;
+        time = newTime;
+
         simpleUpdateGame();
         continuousUpdateGame();
+        hybridUpdateGame();
 
         BeginDrawing();
         ClearBackground(BLACK);
         simpleRenderGame();
         continuousRenderGame();
+        hybridRenderGame();
 
         drawGrid();
         // draw FPS
